36df6da2a2dc827d2aba014c44ad45630de5db47
Jehiah Czebotar <jehiah@gmail.com>
2012-11-07 18:00:32 +0000

    handle dashes in keys; re-work message parsing

--- gographite_test.go
+++ gographite_test.go
@@ -44,4 +44,35 @@ func TestPacketParse(t *testing.T) {
 	assert.Equal(t, "ms", packet.Modifier)
 	assert.Equal(t, float32(1), packet.Sampling)
 
+	d = []byte("a.key.with-0.dash:4|c")
+	packets = parseMessage(bytes.NewBuffer(d))
+	assert.Equal(t, len(packets), 1)
+	packet = packets[0]
+	assert.Equal(t, "a.key.with-0.dash", packet.Bucket)
+	assert.Equal(t, 4, packet.Value)
+	assert.Equal(t, "c", packet.Modifier)
+	assert.Equal(t, float32(1), packet.Sampling)
+
+	d = []byte("a.key.with-0.dash:4|c\ngauge:3|g")
+	packets = parseMessage(bytes.NewBuffer(d))
+	assert.Equal(t, len(packets), 2)
+	packet = packets[0]
+	assert.Equal(t, "a.key.with-0.dash", packet.Bucket)
+	assert.Equal(t, 4, packet.Value)
+	assert.Equal(t, "c", packet.Modifier)
+	assert.Equal(t, float32(1), packet.Sampling)
+
+	packet = packets[1]
+	assert.Equal(t, "gauge", packet.Bucket)
+	assert.Equal(t, 3, packet.Value)
+	assert.Equal(t, "g", packet.Modifier)
+	assert.Equal(t, float32(1), packet.Sampling)
+
+	d = []byte("a.key.with-0.dash:4\ngauge3|g")
+	packets = parseMessage(bytes.NewBuffer(d))
+	assert.Equal(t, len(packets), 0)
+
+	d = []byte("a.key.with-0.dash:4")
+	packets = parseMessage(bytes.NewBuffer(d))
+	assert.Equal(t, len(packets), 0)
 }
