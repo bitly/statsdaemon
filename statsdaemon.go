6f26c5efc17ad6f390b0d5f86f01621d37b16e44
Naoum Naoumov <naoum@eyeviewdigital.com>
2015-08-21 14:32:29 -0400

    send to multiple graphite addresses

--- statsdaemon.go
+++ statsdaemon.go
@@ -206,56 +206,54 @@ func submit(deadline time.Time) error {
 		return nil
 	}
 
-	client, err := net.Dial("tcp", *graphiteAddress)
-	if err != nil {
-		if *debug {
-			log.Printf("WARNING: resetting counters when in debug mode")
-			processCounters(&buffer, now)
-			processGauges(&buffer, now)
-			processTimers(&buffer, now, percentThreshold)
-			processSets(&buffer, now)
-		}
-		errmsg := fmt.Sprintf("dialing %s failed - %s", *graphiteAddress, err)
-		return errors.New(errmsg)
-	}
-
 	num += processCounters(&buffer, now)
 	num += processGauges(&buffer, now)
 	num += processTimers(&buffer, now, percentThreshold)
 	num += processSets(&buffer, now)
 	if num == 0 {
-		client.Close()
 		return nil
 	}
 
-	go func() error {
-		defer client.Close()
+	addresses := strings.Split(*graphiteAddress, ",") //TODO - do this once in the beginning
+	for _, address := range addresses {
+		go send(address, deadline, buffer, num)
+	}
 
-		err = client.SetDeadline(deadline)
-		if err != nil {
-			return err
-		}
-
-		if *debug {
-			for _, line := range bytes.Split(buffer.Bytes(), []byte("\n")) {
-				if len(line) == 0 {
-					continue
-				}
-				log.Printf("DEBUG: %s", line)
-			}
-		}
-
-		_, err = client.Write(buffer.Bytes())
-		if err != nil {
-			errmsg := fmt.Sprintf("failed to write stats - %s", err)
-			return errors.New(errmsg)
-		}
-
-		log.Printf("sent %d stats to %s", num, *graphiteAddress)
-
-		return nil
-	}()
 	return nil
+}
+
+func send(address string, deadline time.Time, buffer bytes.Buffer, num int64) error {
+	client, err := net.Dial("tcp", address)
+	if err != nil {
+		errmsg := fmt.Sprintf("dialing %s failed - %s", *graphiteAddress, err)
+		return errors.New(errmsg)
+	}
+
+	err = client.SetDeadline(deadline)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	if *debug {
+		for _, line := range bytes.Split(buffer.Bytes(), []byte("\n")) {
+			if len(line) == 0 {
+				continue
+			}
+			log.Printf("DEBUG: %s", line)
+		}
+	}
+
+	_, err = client.Write(buffer.Bytes())
+	if err != nil {
+		errmsg := fmt.Sprintf("failed to write stats - %s", err)
+		return errors.New(errmsg)
+	}
+
+	log.Printf("sent %d stats to %s", num, address)
+
+	return nil
+
 }
 
 func processCounters(buffer *bytes.Buffer, now int64) int64 {
